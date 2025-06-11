db342a27658a87f75c212e706d4c0282d13d63cb
Daniel Dang <dnld1ng@yahoo.com>
2025-06-11 18:07:08 -0400

    Able to generate gemini ai responses

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents='Why is the sky blue?'
+)
+print(response.text)
+print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
+print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
