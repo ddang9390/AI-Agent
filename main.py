6562c6bb0446db04d18f4ccd22370ed18fd3f9d0
Daniel Dang <dnld1ng@yahoo.com>
2025-06-13 10:58:57 -0400

    Takes command line argument for prompt

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+def main():
+    argument = sys.argv[1:]
 
-client = genai.Client(api_key=api_key)
+    load_dotenv()
+    if len(argument) == 1:
+        api_key = os.environ.get("GEMINI_API_KEY")
 
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is the sky blue?'
-)
-print(response.text)
-print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
-print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
+        client = genai.Client(api_key=api_key)
+
+        response = client.models.generate_content(
+            model='gemini-2.0-flash-001', contents=argument[0]
+        )
+        print(response.text)
+        print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
+        print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
+
+    else:
+        print('Please input just 1 prompt in quotation marks')
+
+main()
